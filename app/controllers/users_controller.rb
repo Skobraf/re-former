140541a297336d1325c896d4ccf913b963a563b6
jsgilberto <alv.mtz94@gmail.com>
2018-06-26 17:54:12 +0200

    added edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,30 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path
     else
       render :new
     end
-
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
